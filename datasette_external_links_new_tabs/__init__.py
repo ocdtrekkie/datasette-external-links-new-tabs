57118fb089ac30209608f7ed1f65418e85d6c778
Jacob Weisz <inbox@jacobweisz.com>
2022-10-06 23:04:17 -0500

    Open all links in new tab

--- datasette_external_links_new_tabs/__init__.py
+++ datasette_external_links_new_tabs/__init__.py
@@ -1,1 +1,7 @@
 from datasette import hookimpl
+
+@hookimpl
+def extra_body_script():
+    return {
+        "script": "window.onload = function(){ var a = document.getElementsByTagName('a'); for (var i=0; i<a.length; i++){ a[i].setAttribute('target', '_blank'); } }",
+        }
