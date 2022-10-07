bf24d30861c77255fe059b2231c13047768748e7
Jacob Weisz <inbox@jacobweisz.com>
2022-10-06 23:23:56 -0500

    Open external links in new tab

--- datasette_external_links_new_tabs/__init__.py
+++ datasette_external_links_new_tabs/__init__.py
@@ -3,5 +3,5 @@ from datasette import hookimpl
 @hookimpl
 def extra_body_script():
     return {
-        "script": "window.onload = function(){ var a = document.getElementsByTagName('a'); for (var i=0; i<a.length; i++){ a[i].setAttribute('target', '_blank'); } }",
+        "script": "window.onload = function(){ var a = document.getElementsByTagName('a'); for (var i=0; i<a.length; i++){ if(a[i].hostname != location.hostname) {a[i].setAttribute('target', '_blank'); } } }",
         }
